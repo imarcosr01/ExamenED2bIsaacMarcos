765691405c5f8645a1151fabc6507051f73ef225
Isaac_Marcos <dam08.2024@gmail.com>
2024-05-29 09:29:07 +0200

    proyecto documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,8 +1,20 @@
 package examen;
-
+/**
+ * @author Isaac
+ * @version 1.0
+ * 
+ * 
+ * Al poner una hora en esta clase, te dice si es correcta
+ */
 public class Hora {
 	public static boolean horaValida = true;
-
+/**
+ * Introduces los segundos, minutos y horas y si todo está dentro de los requisitos, te devuelve un "true", si no pues un "false"
+ * @param segundos
+ * @param minutos
+ * @param horas
+ * @return
+ */
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
 			if (horas > 23) {
